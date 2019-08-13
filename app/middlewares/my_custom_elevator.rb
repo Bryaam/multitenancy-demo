e9673a3160f9783b4c1d399b47b8f0ea7f3f1b96
Bryam Pacco <bryam.pacco@gmail.com>
2019-08-13 10:18:53 -0500

    Refactoring elevator

--- app/middlewares/my_custom_elevator.rb
+++ app/middlewares/my_custom_elevator.rb
@@ -4,10 +4,14 @@ class MyCustomElevator < Apartment::Elevators::Generic
   DEFAULT_SCHEMA = 'public'.freeze
 
   def parse_tenant_name(request)
-    warden_user = request.env['rack.session']['warden.user.user.key']
-    uid = warden_user.first[0] unless warden_user.nil?
-
-    current_user = User.find_by(id: uid)
+    current_user = User.find_by(id: warden_user_id(request))
     current_user.nil? ? DEFAULT_SCHEMA : current_user.domain
+  end
+
+  protected
+
+  def warden_user_id(request)
+    warden_user = request.env['rack.session']['warden.user.user.key']
+    warden_user.first[0] unless warden_user.nil?
   end
 end
